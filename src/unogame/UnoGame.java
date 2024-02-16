ef062c2e0f895077173aa0f781132e23d7c81f23
amrinder <amrinder@10.48.10.14>
2024-02-16 13:40:31 -0500

    initial commit 2

--- src/unogame/UnoGame.java
+++ src/unogame/UnoGame.java
@@ -10,13 +10,32 @@ package unogame;
  * editor Gurkirat Singh
  and lovish 
  */
+// License and template information can be customized as needed.
+
+
+// Main class to start and manage the Uno card game.
 public class UnoGame {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UnoGame game = new UnoGame();
+        game.startGame();
     }
-    
+
+    // Begins and controls the flow of the Uno game.
+    public void startGame() {
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+
+        // Each player draws 7 cards initially.
+        for (int i = 0; i < 7; i++) {
+            player1.pickCard(deck);
+            player2.pickCard(deck);
+        }
+        // Game logic continues...
+    }
 }
+
+
